a7470f352fd55755f9ee4397157a01f468ca154c
Rutuja Shaha <rshaha@adobe.com>
2024-12-24 20:14:04 +0530

    test

--- script.js
+++ script.js
@@ -71,6 +71,7 @@ const acceptInputHandler = function (eventObj) {
     if (userAgentCheck) {
         // Your iOS-specific logic here
         videoElement.muted = false; 
+        videoElement.poster = 'click_play.jpg';
         isPlaying = false;
     }
     else{
